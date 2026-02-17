8293013c14d89d699be6941953c23069bc10493b
Baradhwaj <139044909+Baradhwaj07@users.noreply.github.com>
2026-02-17 19:20:25 +0530

    OOPS with string.join

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * Printing OOPS
+ * @author Baradhwaj V
+ * @version 1.1
+ */
+
+public class OOPSBannerApp{
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "*****", " ", "*****", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*****", " ", "*****", " ", "*    ", " ", "*****"));                
+    }
+}
